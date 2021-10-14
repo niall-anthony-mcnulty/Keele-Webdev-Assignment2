215cbacf9374dad5ece5829ec0fcfef1a8bc875a
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-14 22:10:40 +0800

    Add sub folders plus flavicon

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
         <title>Weather Application</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="css/main.css" type="text/css">
+        <link rel="icon" href="images/data.png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     </head>
     <body>
+        <header>Weather App</header>
         
         
     
