776107c756ca049ecef77199b124c6cdb42c1a29
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-17 23:12:17 +0800

    take away meta tag

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
         <title>Weather Application</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Refresh" content="2; url=public/index.html">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css" type="text/css">
         <link rel="icon" href="images/weather-icon-favicon.png">
