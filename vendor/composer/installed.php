b35359508f01ba2d0ed8fe9e373b6ad007ecf4ea
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-20 02:32:51 +0800

    heroku stuff

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd11397450d32f23c1c9d014bb0a976191c33b9aa',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd11397450d32f23c1c9d014bb0a976191c33b9aa',
+            'dev_requirement' => false,
+        ),
+    ),
+);
