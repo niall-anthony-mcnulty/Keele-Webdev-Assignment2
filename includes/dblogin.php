b910508c36cd4b242edd82a0d2b8c632e3753d05
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-19 19:04:15 +0800

    sql working but no entries

--- includes/dblogin.php
+++ includes/dblogin.php
@@ -1,14 +1,15 @@
 
 <?php // login.php
 
-    $hn = '127.0.0.1';
-    $un = 'staff';
-    $pw = 'admin123';
-    $db = 'WEATHER';
-    $conn = new mysqli($hn, $un, $pw, $db);
+$hn = '127.0.0.1';
+$un = 'staff';
+$pw = 'admin123';
+$db = 'WEATHER';
+$conn = new mysqli($hn, $un, $pw, $db);
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    
-    ?>
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    echo('FAILED TO OONNECT');
+}
+
+?>
