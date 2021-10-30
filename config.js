674e2dededfd467a0916b141b9a93d61b4520eac
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-30 15:52:52 +0800

    hide key

--- config.js
+++
@@ -1,2 +0,0 @@
-var config = {WEATHER_API_KEY : 'ca3c995a6eebe56bd7a6d2e8dfbf1f55'
-}
--- index.php
+++ index.php
@@ -16,8 +16,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>  
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script type='text/javascript' src='config.js'></script>
     <script src="js/main.js"></script>
+    <script src="js/config.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
 </head>
 <body>
